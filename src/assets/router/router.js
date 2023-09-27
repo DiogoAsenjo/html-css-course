65f186d529bddb235beab7ca1f89ffb15d00fcd6
DiogoAsenjo <diogoasenjo@outlook.com>
2023-09-27 09:41:49 -0300

    Criando a segunda página do projeto

--- src/assets/router/router.js
+++ src/assets/router/router.js
@@ -1,11 +1,13 @@
 import { HashRouter, Route, Routes } from "react-router-dom";
 import Homepage from "../../pages/homepage/homepage";
+import Summary from "../../pages/summary/summary";
 
 const Router = () => {
   return (
     <HashRouter>
       <Routes>
         <Route index element={<Homepage />} />
+        {/* <Route index element={<Summary />} /> */}
       </Routes>
     </HashRouter>
   );
---
+++ src/pages/summary/summary.js
@@ -0,0 +1,11 @@
+import "./summary.css";
+
+function Summary() {
+  return (
+    <div>
+      <h1>Here you can check all the topics that I already learned:</h1>
+    </div>
+  );
+}
+
+export default Summary;
