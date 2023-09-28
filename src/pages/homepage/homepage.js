b32dea6b538080f6ffc388eefb6151f319547aae
DiogoAsenjo <diogoasenjo@outlook.com>
2023-09-28 11:59:59 -0300

    Mudando o CSS do projeto

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function Homepage() {
   return (
     <div>
-      <h1>HTML and CSS Notebook</h1>;
+      <h1>HTML and CSS Notebook</h1>
       <p>
         I will use this repo and this React project as part of my HTML and CSS
         studies. I already have some basic knowledge. Right know I consider
